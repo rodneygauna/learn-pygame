f381fcda8e18e0dcc102bd0a2c4e716fe9694513
Rodney Gauna <rodneygauna@gmail.com>
2022-10-26 13:22:20 -0700

    added ducks to the scene display

--- duck_shooter/duck_shooter.py
+++ duck_shooter/duck_shooter.py
@@ -9,6 +9,7 @@ A duck shooter game following the Udemy course
 # -----------------------------------------------------------------------------
 import sys
 import os
+import random
 import pygame
 
 
@@ -45,12 +46,23 @@ crosshair_rect = crosshair.get_rect(center=(640, 360))
 duck_surface = pygame.image.load(
     os.path.join("duck_shooter/assets", "duck.png"))
 
+# Land Variables
 LAND_POSITION_Y = 560
 LAND_SPEED = 1
 
+# Water Variables
 WATER_POSITION_Y = 640
 WATER_SPEED = 1.5
 
+
+# Duck Variables
+DUCK_LIST = []
+for duck in range(20):
+    duck_position_x = random.randrange(50, 1200)
+    duck_position_y = random.randrange(120, 600)
+    duck_rect = duck_surface.get_rect(
+        center=(duck_position_x, duck_position_y))
+    DUCK_LIST.append(duck_rect)
 
 # -----------------------------------------------------------------------------
 # Pygame Game Loop
@@ -78,6 +90,10 @@ while True:
     if WATER_POSITION_Y <= 620 or WATER_POSITION_Y >= 680:
         WATER_SPEED *= -1
     screen.blit(water_bg, (0, WATER_POSITION_Y))
+
+    # Duck positioning
+    for duck_rect in DUCK_LIST:
+        screen.blit(duck_surface, duck_rect)
 
     # Crosshair positioning
     screen.blit(crosshair, crosshair_rect)
